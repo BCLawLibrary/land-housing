a2a9325fb0fbfe46a815e871394c37c31943203e
sengutengu <seungkim.h@gmail.com>
2026-04-21 11:09:50 -0400

    Initial commit

---
+++ js/land-housing.js
@@ -0,0 +1,103 @@
+async function fetchCSVData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("Failed to fetch CSV");
+    return Papa.parse(await response.text(), {
+      header: true,
+      skipEmptyLines: true,
+    });
+  } catch (error) {
+    console.error(error);
+    return { data: [] }; // Return empty data in case of error
+  }
+}
+
+function formatWork(rowData) {
+  let [
+    Section,
+    Authorship,
+    Title,
+    Keywords,
+    Year,
+    Month,
+    Date,
+    Thumbnail,
+    Link,
+  ] = rowData;
+}
+
+async function convertDataToObject(url) {
+  const CSVdata = await fetchCSVData(url);
+  return CSVdata.data;
+}
+
+function initializeTable(data) {
+  const custom_columns = [
+    { title: "Section", data: "Section", visible: false },
+    { title: "Authorship", data: "Authorship", visible: false },
+    { title: "Title", data: "Title", visible: false },
+    { title: "Keywords", data: "Keywords", visible: false },
+    { title: "Year", data: "Year", visible: false },
+    { title: "Month", data: "Month", visible: false },
+    { title: "Date", data: "Date", visible: false },
+    { title: "Thumbnail", data: "Thumbnail", visible: false },
+    { title: "Link", data: "Link", visible: false },
+    {
+      title: "Works",
+      data: null,
+      render: function (data, type, row) {
+        return `
+        <div class="card">
+          <div class="card-thumbnail">
+            <img src="${row.Thumbnail}" />
+          </div>
+          <div class="card-text">
+            <div class="card-date">
+              ${row.Month}/${row.Date}/${row.Year}
+            </div>
+            <div class="card-title">
+              ${row.Title}
+            </div>
+            <div class="card-authorship">
+              ${row.Authorship}
+            </div>
+            <div class="card-keywords">
+            ${row.Keywords}
+            </div>
+          </div>
+        </div>
+        `;
+      },
+    },
+  ];
+
+  const table = new DataTable("#works__table", {
+    dom: "Brt", // buttons, processing, table
+    autoWidth: false,
+    paging: false,
+    data: data,
+    columns: custom_columns,
+    order: [
+      [3, "desc"],
+      [1, "asc"],
+      [0, "asc"],
+    ],
+    rowGroup: { dataSrc: "Section" }, // relies on RowGroup extension
+  });
+
+  return table;
+}
+
+$(document).ready(function () {
+  async function main() {
+    const url =
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSvcazCOMPpKONojpgN19KIaqRBEApjYaeyHdB3DKDC9QI8TyWBTMki-vw4fPCHt2HfUQYX-gfj8DUM/pub?gid=0&single=true&output=csv";
+    const data = await convertDataToObject(url);
+
+    var table = initializeTable(data);
+
+    $(".policies__loading").hide();
+  }
+
+  main();
+});
